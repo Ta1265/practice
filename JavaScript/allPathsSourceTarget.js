131011e7b6dff2417ad1d3b2f0e6d0ebe8106a89
Taylor Anderson <tayloranderson1265@gmail.com>
2021-03-08 14:46:51 -0500

    completed graph toy problem

---
+++ JavaScript/allPathsSourceTarget.js
@@ -0,0 +1,79 @@
+/* eslint-disable func-names */
+/* eslint-disable no-var */
+/*
+797. All Paths From Source to Target
+
+Given a directed acyclic graph (DAG) of n nodes
+ labeled from 0 to n - 1, find all possible paths from node 0
+  to node n - 1, and return them in any order.
+
+The graph is given as follows: graph[i] is a
+list of all nodes you can visit from node i
+ (i.e., there is a directed edge from node i to node graph[i][j]).
+
+Example 1:
+
+Input: graph = [[1,2],[3],[3],[]]
+Output: [[0,1,3],[0,2,3]]
+Explanation: There are two paths: 0 -> 1 -> 3 and 0 -> 2 -> 3.
+Example 2:
+
+Input: graph = [[4,3,1],[3,2,4],[3],[4],[]]
+Output: [[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+Example 3:
+
+Input: graph = [[1],[]]
+Output: [[0,1]]
+Example 4:
+
+Input: graph = [[1,2,3],[2],[3],[]]
+Output: [[0,1,2,3],[0,2,3],[0,3]]
+Example 5:
+
+Input: graph = [[1,3],[2],[3],[]]
+Output: [[0,1,2,3],[0,3]]
+
+Constraints:
+
+n == graph.length
+2 <= n <= 15
+0 <= graph[i][j] < n
+graph[i][j] != i (i.e., there will be no self-loops).
+The input graph is guaranteed to be a DAG.
+
+*/
+
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+var allPathsSourceTarget = function (graph) {
+  const paths = [];
+  const recurse = (v, curPath) => {
+    if (v === graph.length - 1) return paths.push(curPath);
+    for (let e = 0; e < graph[v].length; e += 1) {
+      recurse(graph[v][e], [...curPath, graph[v][e]]);
+    }
+  };
+  recurse(0, [0]);
+  return paths;
+};
+
+function check(arr1, arr2) {
+  const strArr1 = arr1.map((arr) => arr.toString());
+  const strArr2 = arr2.map((arr) => arr.toString());
+  for (let i = 0; i < strArr1.length; i += 1) {
+    if (strArr2.indexOf(strArr1[i]) < 0) return false;
+  }
+  return true;
+}
+
+(function () {
+  console.log(check(allPathsSourceTarget([[1, 2], [3], [3], []]), [[0, 1, 3], [0, 2, 3]]));
+  console.log(
+    check(
+      allPathsSourceTarget([[4, 3, 1], [3, 2, 4], [3], [4], []]),
+      [[0, 4], [0, 3, 4], [0, 1, 3, 4], [0, 1, 2, 3, 4], [0, 1, 4]],
+    ),
+  );
+}());
